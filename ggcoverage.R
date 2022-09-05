217f113716663ffc59df7f4f841c5e2b5107fbba
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-09-05 15:24:36 +0200

    importing data

--- ggcoverage.R
+++ ggcoverage.R
@@ -2,6 +2,19 @@
 # last updated 2022-09-05
 # https://showteeth.github.io/ggcoverage/articles/ggcoverage.html
 
-# Install package ----
+# Install packages ----
 
 remotes::install_github("showteeth/ggcoverage")
+# BiocManager::install("rtracklayer")
+# BiocManager::install("ggbio")
+# BiocManager::install("rtracklayer")
+# Load packages ----
+
+library(rtracklayer)
+library(ggcoverage)
+
+# RNA-seq data ----
+
+# load metadata
+meta.file <- system.file("extdata", "RNA-seq", "meta_info.csv", package = "ggcoverage")
+sample.meta <- read.csv(meta.file)
