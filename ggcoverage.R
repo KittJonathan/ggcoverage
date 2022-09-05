69fa60aed4c069ebf886fab0d1981612f0431467
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-09-05 15:14:02 +0200

    initial commit

---
+++ ggcoverage.R
@@ -0,0 +1,7 @@
+# ggcoverage
+# last updated 2022-09-05
+# https://showteeth.github.io/ggcoverage/articles/ggcoverage.html
+
+# Install package ----
+
+remotes::install_github("showteeth/ggcoverage")
